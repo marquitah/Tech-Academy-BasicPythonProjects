74661a7488575f2201fbf4e0058a4fc37a28fa25
Marquita <Marquita.dec@gmail.com>
2019-08-08 15:44:53 -0600

    Add files via upload

--- FileIterateDrill.py
+++ FileIterateDrill.py
@@ -5,13 +5,14 @@ import os
 #Your script will need to use the listdir() method from
 #the OS module to iterate through all files within a specific directory.
 
-files = '/IterateFileDrill'
-
-for files in os.listdir('/IterateFileDrill'):
-    print(files)
-
 #Your script will need to use the path.join() method from the OS module
 #to concatenate the file name to its file path, forming an absolute path.
+
+#Your script will need to use the getmtime() method from the OS module
+#to find the latest date that each text file has been created or modified.
+
+#Your script will need to print each file ending with a “.txt” file extension
+#and its corresponding mtime to the console.
 
 fName = '1.rtf'
 
@@ -21,21 +22,15 @@ gName = '2.rtf'
 
 gPath = '/IterateFileDrill'
 
-joinPath = os.path.join(fPath,fName)
-print(joinPath)
 
-joinPath2 = os.path.join(gPath,gName)
-print(joinPath2)
+files = '/IterateFileDrill'
 
-#Your script will need to use the getmtime() method from the OS module
-#to find the latest date that each text file has been created or modified.
 
-pathTime1 = os.path.getmtime(joinPath)
-pathTime2 = os.path.getmtime(joinPath2)
 
-#Your script will need to print each file ending with a “.txt” file extension
-#and its corresponding mtime to the console.
-
-print(joinPath,pathTime1)
-print(joinPath2,pathTime2)
-
+for files in os.listdir('/IterateFileDrill'):
+    if files.endswith(".rtf"):
+        joinPath = os.path.join(fPath,fName)
+        joinPath2 = os.path.join(gPath,gName)
+        pathTime1 = os.path.getmtime(joinPath)
+        pathTime2 = os.path.getmtime(joinPath2)
+        print(joinPath,pathTime1,joinPath2,pathTime2)
