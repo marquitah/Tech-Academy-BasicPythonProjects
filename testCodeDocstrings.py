f08b867810956f60da1f8a81f8aefd710ff65f5f
Marquita <Marquita.dec@gmail.com>
2019-08-08 11:38:35 -0600

    Add files via upload

---
+++ testCodeDocstrings.py
@@ -0,0 +1,14 @@
+# How to comment out in Python
+
+"""this is how to comment out a longer note
+on more than two lines
+this is three lines
+"""
+
+def printMe():
+    """this is my docstring
+    """
+
+    print("Meeee!!!!")
+
+print(printMe.__doc__)
